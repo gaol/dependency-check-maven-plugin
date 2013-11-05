0c6754c219f0bc067aa7389fab4effe972c29c51
Lin Gao <lgao@redhat.com>
2013-11-05 17:56:45 +0800

    First operational version

--- README.md
+++ README.md
@@ -1,4 +1,14 @@
-maven-dep-check
-===============
+Dependency Check Maven Plugin
+=============================
 
-A tool to check which maven repository one or multiple dependencies belong to
+A Maven Plugin used to check which dependencies or plugins are available in which remote repository
+
+
+There is a <b>maven-settings.xml</b> file in this source code tree, when you want to use this maven plugin, please download this file
+to your local machine, and then in your project root directory, run:
+<pre>
+   mvn -s maven-settings.xml dependency-check:check
+</pre>
+
+The default action will list all dependencies which are not in Maven Central Repository
+
--- src/main/java/org/jboss/maven/plugins/dependency/DependencyCheckMojo.java
+++ src/main/java/org/jboss/maven/plugins/dependency/DependencyCheckMojo.java
@@ -3,6 +3,13 @@
  */
 package org.jboss.maven.plugins.dependency;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -16,6 +23,7 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.plugins.annotations.ResolutionScope;
 import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.IOUtil;
 
 /**
  * @author lgao
@@ -24,6 +32,12 @@ import org.apache.maven.project.MavenProject;
 @Mojo( name = "check", requiresDependencyResolution = ResolutionScope.TEST, threadSafe = true )
 public class DependencyCheckMojo extends AbstractMojo
 {
+   private static final List<String> excludedGroupIds = new ArrayList<String>();
+   static
+   {
+      excludedGroupIds.add("org.apache.maven.plugins");
+   }
+   
    // fields -----------------------------------------------------------------
 
    /**
@@ -32,31 +46,146 @@ public class DependencyCheckMojo extends AbstractMojo
    @Component
    private MavenProject project;
    
+   /**
+    * Which repository do you want to check against. Default is the maven central repository
+    * The value is one of predefined remote repositories in the current active profiles.
+    * 
+    */
    @Parameter(property = "repoId", defaultValue = "central")
    private String repoId;
    
+   
+   /**
+    * The output file which contains list of missing dependencies or plugins. Default is deps-list.txt in the target directory.
+    * 
+    */
+   @Parameter(property = "output")
+   private File output;
+   
    public void execute() throws MojoExecutionException, MojoFailureException
    {
-      // TODO Auto-generated method stub
-      Set<Artifact> artifacts = project.getArtifacts();
       
-      for (Artifact artifact: artifacts)
+      String projectGroupId = project.getGroupId();
+      
+      // dependencies
+      Set<Artifact> dependencies = project.getArtifacts();
+      
+      // plugins
+      Set<Artifact> plugins = project.getPluginArtifacts();
+      
+      // collect all artifacts needed
+      Set<Artifact> artifacts = new HashSet<Artifact>();
+      artifacts.addAll(dependencies);
+      artifacts.addAll(plugins);
+      
+      if (output == null)
       {
-         
-         getLog().info(artifact.getGroupId() + ":" + artifact.getArtifactId() + ":" + artifact.getVersion() + ":" + artifact.getScope());
+         output = new File(project.getBasedir() + "/target/deps-list.txt");
+      }
+      if (!output.getParentFile().exists())
+      {
+         output.getParentFile().mkdirs();
+      }
+      getLog().debug("Will record missing artifacts into: " + output.getAbsolutePath());
+
+      ArtifactRepository repo = getArtifactRepository();
+      if (repo == null)
+      {
+         throw new MojoFailureException("Unkown repository: " + this.repoId);
       }
       
+      getLog().debug("Checking against repository: " + repo.getId());
+      
+      List<String> missingArtifacts = new ArrayList<String>();
+      for (Artifact artifact: artifacts)
+      {
+         String groupId = artifact.getGroupId();
+         if (projectGroupId.equals(groupId) || excludedGroupIds.contains(groupId))
+         {
+            getLog().debug("GroupId: " + groupId + " will be skipped.");
+            continue;
+         }
+         String repoURL = repo.getUrl();
+         if (!repoURL.endsWith("/"))
+         {
+            repoURL = repoURL + "/";
+         }
+         String artifactLink = repoURL + artifact.getGroupId().replaceAll("\\.", "/") + "/" + artifact.getArtifactId() + "/" + artifact.getVersion() + "/" + artifact.getArtifactId() + "-" + artifact.getVersion() + ".pom";
+         getLog().debug("Checking artifact: " + gatv(artifact) + " at: " + artifactLink);
+         HttpURLConnection urlConn = null;
+         try
+         {
+            URL linkURL = new URL(artifactLink);
+            urlConn = (HttpURLConnection)linkURL.openConnection();
+            urlConn.connect();
+            int code = urlConn.getResponseCode();
+            if (code >= 400)
+            {
+               getLog().debug("\tNot found.");
+               missingArtifacts.add(gatv(artifact));
+            }
+         }
+         catch (IOException e)
+         {
+            getLog().warn("Wrong link: " + artifactLink, e);
+         }
+         finally
+         {
+            urlConn.disconnect();
+         }
+      }
+      
+      PrintWriter writer = null;
+      try
+      {
+         writer = new PrintWriter(output);
+         for (String artiStr: missingArtifacts)
+         {
+            getLog().debug(artiStr);
+            writer.println(artiStr);
+         }
+      }
+      catch (IOException e)
+      {
+         getLog().warn("Error to write data into file: " + output.getAbsolutePath(), e);
+      }
+      finally
+      {
+         IOUtil.close(writer);
+      }
+   }
+   
+   /**
+    * Gets current ArtifactRepository which will be used to check against
+    */
+   private ArtifactRepository getArtifactRepository()
+   {
       List<ArtifactRepository> artifactRepos = project.getRemoteArtifactRepositories();
       if (artifactRepos != null && artifactRepos.size() > 0)
       {
          for (ArtifactRepository artiRepo: artifactRepos)
          {
-            getLog().info("Repo: " + artiRepo.getId() + ":" + artiRepo.getUrl());
+            if (artiRepo.getId().equals(this.repoId))
+            {
+               return artiRepo;
+            }
          }
       }
-      
-      
-      
+      return null;
+   }
+   
+   
+   private String gatv(Artifact artifact)
+   {
+      StringBuilder sb = new StringBuilder();
+      sb.append(artifact.getGroupId());
+      sb.append(":");
+      sb.append(artifact.getArtifactId());
+      sb.append(":");
+      sb.append(artifact.getType());
+      sb.append(":");
+      sb.append(artifact.getVersion());
+      return sb.toString();
    }
 
 }
